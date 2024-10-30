8f6f305c79a45358f3b911291d0bd3ff9140c38e
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-30 06:56:17 +0100

    feat(constants): add geolocation url

--- constants.ts
+++ constants.ts
@@ -1,4 +1,5 @@
 export const API_URL = process.env.EXPO_PUBLIC_API_URL;
 export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
-export const CURRENT_WEATHER_URL = `${API_URL}/weather`;
-export const FORECAST_WEATHER_URL = `${API_URL}/forecast`;
+export const CURRENT_WEATHER_URL = `${API_URL}/data/2.5/weather`;
+export const FORECAST_WEATHER_URL = `${API_URL}/data/2.5/forecast`;
+export const GEO_WEATHER_URL = `${API_URL}/geo/1.0/direct`;
