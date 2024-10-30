e07cd2d65b633d92658543a77a138b8fc222c0dc
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-30 06:30:56 +0100

    feat(constants): add api and url constants

--- constants.ts
+++ constants.ts
@@ -1,2 +1,4 @@
 export const API_URL = process.env.EXPO_PUBLIC_API_URL;
 export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+export const CURRENT_WEATHER_URL = `${API_URL}/weather`;
+export const FORECAST_WEATHER_URL = `${API_URL}/forecast`;
